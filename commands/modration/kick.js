ddc04ee59b553ed2960c6c3ec6a086d967bf75a8
YASHOFFICAL <70049117+YASHOFFICAL@users.noreply.github.com>
2020-12-04 14:47:01 +0530

    Update kick.js

--- commands/modration/kick.js
+++ commands/modration/kick.js
@@ -4,7 +4,7 @@ module.exports = {
   name: "kick",
   category: "moderation",
   description: "Kick anyone with one shot xD",
-  usage: "kick <@user> <raeson>",
+  usage: "kick <@user> <reason>",
   run: (client, message, args) => {
     
     if(!message.member.hasPermission("KICK_MEMBERS")) {
