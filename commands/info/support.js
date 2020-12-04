92132228e677ce7ec9d28f7c9e0b1fac68674e34
YASHOFFICAL <70049117+YASHOFFICAL@users.noreply.github.com>
2020-12-04 15:13:27 +0530

    Create support.js

---
+++ commands/info/support.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: "support server",
+    category: "info",
+    description: "server to get support ",
+    usage: "support server",
+    run: (client, message) => {
+      message.channel.send("https://discord.gg/p4yVG35Zzt");
+    }
+    
+  }
