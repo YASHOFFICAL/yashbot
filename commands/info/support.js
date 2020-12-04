7ccc8b7cb54c9d4c2db927838fadcaa93d07666b
YASHOFFICAL <70049117+YASHOFFICAL@users.noreply.github.com>
2020-12-04 15:18:19 +0530

    Update support.js

--- commands/info/support.js
+++ commands/info/support.js
@@ -2,6 +2,7 @@ module.exports = {
     name: "supportserver",
     category: "info",
     description: "server to get support ",
+    aliases: ["ss"],
     usage: "supportserver",
     run: (client, message) => {
       message.channel.send("https://discord.gg/p4yVG35Zzt");
