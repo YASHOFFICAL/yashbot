6555afed6e3a0d495a9e43142e24034b2ecc516e
YASHOFFICAL <70049117+YASHOFFICAL@users.noreply.github.com>
2020-12-05 20:15:03 +0530

    Update avatar.js

--- commands/info/avatar.js
+++ commands/info/avatar.js
@@ -1,5 +1,4 @@
 const { MessageEmbed } = require("discord.js");
-const { formatDate } = require("../../functions");
 module.exports = {
   name: "avatar",
   description: "Get your own or someone else's avatar",
@@ -15,11 +14,7 @@ module.exports = {
       Embed.setTitle(`Your avatar!`);
       Embed.setThumbnail(message.author.displayAvatarURL());
       Embed.setColor(`RANDOM`);
-      Embed.setDescription(
-        `Joined: (MM/DD/YYYY) ${formatDate(message.member.joinedAt)}\nID: ${
-          message.author.id
-        }\nRoles: ${roles}`
-      );
+      Embed.setDescription();
       return message.channel.send(Embed);
     } else {
       let User = message.mentions.members.first();
@@ -29,11 +24,7 @@ module.exports = {
       Embed.setTitle(`${bot.users.cache.get(User.id).tag}'s avatar!`);
       Embed.setThumbnail(bot.users.cache.get(User.id).displayAvatarURL());
       Embed.setColor(`RANDOM`);
-      Embed.setDescription(
-        `Joined: (MM/DD/YYYY) ${formatDate(User.joinedAt)}\nID: ${
-          User.id
-        }\nRoles: ${roles}`
-      );
+      Embed.setDescription();
       return message.channel.send(Embed);
     }
   },
