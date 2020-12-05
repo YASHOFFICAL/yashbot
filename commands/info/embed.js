fbd2bf347cdae0811252e69e3a659bee6e287e0d
YASHOFFICAL <70049117+YASHOFFICAL@users.noreply.github.com>
2020-12-05 18:23:13 +0530

    Create embed.js

---
+++ commands/info/embed.js
@@ -0,0 +1,23 @@
+const { MessageEmbed } = require("discord.js")
+
+
+module.exports = {
+  name: "embed",
+  usage: "embed <#colourcode> <message>",
+  description: "embed your message",
+  category: "main",
+  run: (client, message, args) => {
+    
+    if(!args.length) {
+      return message.channel.send("please give message")
+    }
+    
+    
+    let embed = new MessageEmbed()
+    .setColor(" ")
+    .setDescription(args.join(" "))
+    
+       message.channel.send("done")
+    
+  }
+}
