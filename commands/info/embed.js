4516e0e49f5cdb2b1556389b6507520581efa458
YASHOFFICAL <70049117+YASHOFFICAL@users.noreply.github.com>
2020-12-05 19:09:06 +0530

    Update embed.js

--- commands/info/embed.js
+++ commands/info/embed.js
@@ -7,12 +7,8 @@ module.exports = {
   description: "embed your message",
   category: "main",
   run: async (client, message, args) => {
-      if (!args[0]) return message.chennal.send(```${prefix}embed [chn] <color> <text>
-             ^^^^^^^
-color is a required argument that is missing.```)
-      if (!args[1]) return message.chennal.send(```${prefix}embed [chn] <color> <text>
-                     ^^^^^^
-text is a required argument that is missing.```)
+      if (!args[0]) return message.chennal.send("color is a required argument that is missing")
+      if (!args[1]) return message.chennal.send("text is a required argument that is missing")
       const Embed = new Discord.MessageEmbed()
           .setDescription(args[2])
           .setColor(args[1])
