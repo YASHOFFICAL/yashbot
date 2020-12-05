0bceec9f5169e8acfadf1337693a6a7db0152651
YASHOFFICAL <70049117+YASHOFFICAL@users.noreply.github.com>
2020-12-05 21:26:39 +0530

    Update announce.js

--- commands/info/announce.js
+++ commands/info/announce.js
@@ -2,12 +2,11 @@ const { MessageEmbed } = require("discord.js");
 module.exports = {
   name: "announce",
   description: "Get the bot to say what ever you want in a specific channel.",
-  usage: "<channel id> <msg>",
+  usage: "<msg>",
   run: async (bot, message, args) => {
     
     let MSG = message.content
-      .split(`${bot.prefix}announce ${rChannel.id} `)
-      .join("");
+    
     if (!MSG)
       return message.channel.send(`You did not specify your message to send!`);
     const _ = new MessageEmbed()
